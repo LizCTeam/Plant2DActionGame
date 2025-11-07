7520dcd1095633e1303c718258066de5c33e0bbe
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2025-11-07 12:30:13 +0900

    HitboxとHurtboxの追加

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : BasicBehaviour
+{
+    public int damage;
+
+    public GameObject owner;
+
+    public bool isContinuousDamage;
+    
+    private readonly Dictionary<Collider2D, Coroutine> _hitEvents = new();
+    
+    // 注意OnTriggerEnterではなくOnTriggerEnter"2D" (1敗)
+    // Unity上でIs TriggerをONにすることでGodotのArea2Dみたいな使い方が出来る
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isContinuousDamage)
+        {
+            // 持続ダメージの例、ここ一旦スキップしてもいい
+            DoMultipleHit(other);
+        }
+        else
+        {
+            // 通常の使い方、持続ダメージがない
+            DoBasicHit(other);   
+        }
+    }
+
+    private void DoBasicHit(Collider2D other)
+    {
+        var hurtbox = other.GetComponent<Hurtbox>();
+        hurtbox?.Hurt(damage);
+    }
+
+    private void DoMultipleHit(Collider2D other)
+    {
+        var hurtbox = other.GetComponent<Hurtbox>();
+        if (hurtbox ==null) return;
+        
+        // Coroutineは途中で止めてまた続けられる処理
+        Coroutine coroutine = StartCoroutine(DamageCoroutine(hurtbox));
+        // 複数のHurtboxが入る可能性があるからDictionaryを使ってCoroutineを記録する
+        _hitEvents.Add(other, coroutine);
+    }
+    
+    // IEnumeratorを返す関数はCoroutineの宣言
+    private IEnumerator DamageCoroutine(Hurtbox hurtbox)
+    {
+        while (true)
+        {
+            // yield return使うことによってこの関数を一時停止出来る
+            hurtbox?.Hurt(damage);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (_hitEvents.ContainsKey(other))
+        {
+            // 該当Hurtboxが離れたらDictionaryに記録したCoroutineを停止させる
+            StopCoroutine(_hitEvents[other]);
+            _hitEvents.Remove(other);
+        }
+    }
+}
---
+++ Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class Hurtbox : BasicBehaviour
+{
+    public GameObject owner;
+    // IDamageableはインターフェース、このインターフェースを継承したclassは必ず
+    // OnDamaged関数を実装しなくちゃいけない
+    // これによってオブジェクト、ボス、敵同じHurtboxを使っても違う表現が出来る
+    private IDamageable _damageable;
+
+    private void Awake()
+    {
+        if (owner)
+        {
+            // GetComponents系は遅いからAwakeでやる
+            _damageable = owner.GetComponents(typeof(IDamageable)).FirstOrDefault() as IDamageable;
+        }
+    }
+
+    public void Hurt(int damage)
+    {
+        // ownerがIDamageableであればダメージを与える
+        _damageable?.OnDamaged(damage);
+    }
+}
