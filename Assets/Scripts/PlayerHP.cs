4bbc0a977dd8ea1f4643bf5b6231d6f10bce67e1
kawaharu0101 <kawaharu180103@gmail.com>
2025-11-29 17:39:08 +0900

    HPUIの再修正

--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHP : MonoBehaviour
+public class PlayerHP : BasicBehaviour
 {
     [SerializeField, Header("HPアイコン")]
     private GameObject playerIcon;
 
     [SerializeField] private Player player;
     private int beforeHp;
+    
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+        //プレイヤーのスクリプトを取得(多分)
+        //player = GetComponent<Player>();
+    }
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void OnStart()
     {
+        base.OnStart();
         //プレイヤーのスクリプトを取得(多分)
         //player = GetComponent<Player>();
         beforeHp = player.GetHp();
@@ -24,36 +33,54 @@ public class PlayerHP : MonoBehaviour
         //プレイヤーのHP分アイコンを生成
         for (int i = 0; i < player.GetHp(); i++)
         {
-            GameObject playerHPObj = Instantiate(playerIcon,transform);
+            GameObject playerHPObj = Instantiate(playerIcon, transform);
             //GameObject playerHPObj = Instantiate(playerIcon);
             //playerHPObj.transform.SetParent(transform);//親(HPオブジェクト)に設定
         }
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void OnUpdate()
     {
+        base.OnUpdate();
         ShowHPIcon();
     }
 
     private void ShowHPIcon()
     {
-
+        int currentHp = player.GetHp();
 
         //HPが変化しなければ何もしない
-        if (beforeHp == player.GetHp()) return;
+        if (beforeHp == currentHp) return;
 
-        //HPが減った場合
         //すべてのHPアイコンを取得
         Image[] icon = transform.GetComponentsInChildren<Image>();
 
-        //現在のHP以下のアイコンのみ表示、それ以外は非表示
-        for (int i = 0; i < icon.Length; i++)
+
+
+        if (currentHp < beforeHp)
         {
-            icon[i].gameObject.SetActive(i < player.GetHp());
+            //HPが減った場合
+            //現在のHP以下のアイコンのみ表示、それ以外は非表示
+            for (int i = 0; i < icon.Length; i++)
+            {
+                icon[i].gameObject.SetActive(i < currentHp);
+            }
+
+            
         }
+        //else if (currentHp > beforeHp)
+        //{
+        //    int diff = currentHp - beforeHp;
+        //    for (int i = 0; i < diff; i++)
+        //    {
+        //        //HPが増えた場合、アイコンを追加生成
+        //        GameObject playerHpObj = Instantiate(playerIcon, transform);
+        //    }
+        //}
+
 
         //現在のHPを保存
-        beforeHp = player.GetHp();
+        beforeHp = currentHp;
     }
 }
