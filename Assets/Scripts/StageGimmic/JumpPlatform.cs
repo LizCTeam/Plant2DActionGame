5b540ef9c46b31732f8e39421c95ac7cc68d4375
JNneko-S <blw3591@gmail.com>
2025-12-01 23:29:06 +0900

    ジャンプ台の修正

--- Assets/Scripts/StageGimmic/JumpPlatform.cs
+++ Assets/Scripts/StageGimmic/JumpPlatform.cs
@@ -3,12 +3,8 @@ using UnityEngine.Serialization;
 
 public class JumpPlatform : BasicBehaviour
 {
-    [FormerlySerializedAs("player")] [SerializeField] 
-    private GameObject playerObject;
     [SerializeField]
     private float _jumpForce = 0f;
-
-    private float _playerJumpSpeed;
     
     protected override void OnStart()
     {
