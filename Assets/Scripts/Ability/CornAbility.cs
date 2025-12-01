f336b088899d4079392c21f2d1b434a838701869
JNneko-S <blw3591@gmail.com>
2025-12-01 23:45:03 +0900

    コーンの左右反転修正

--- Assets/Scripts/Ability/CornAbility.cs
+++ Assets/Scripts/Ability/CornAbility.cs
@@ -10,6 +10,9 @@ public class CornAbility : BasicBehaviour, IHasAbility
     {
         Debug.Log("Used Ability 2");
         var corn = Instantiate(CornObject, Position.transform.position, Quaternion.identity);
+        var scale = corn.transform.localScale;
+        scale.x = Mathf.Sign(Parent.transform.localScale.x) * Mathf.Abs(Parent.transform.localScale.x);
+        corn.transform.localScale = scale;
         corn.transform.parent = Parent.transform;
     }
 }
